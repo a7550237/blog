cc34f4e5a3e901c65071e3d218c1d68e133920a7
guolongjie <1084665773@qq.com>
2019-06-30 15:21:01 +0800

    test commit

---
+++ src/main/java/com/glj/blog/config/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.glj.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author guolongjie
+ * @since 2019/6/30
+ */
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+
+
+}
